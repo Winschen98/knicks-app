92bc4591c979bac962e24c7bb4fc955d6e15219e
winschen98 <winston.chen98@gmail.com>
2021-03-10 15:02:45 -0800

    Completed basic Nav-bar appearance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import './styled-components/App.css';
 import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        test
+      <header className='header'>
         <Nav />
       </header>
+      <main className='main'>
+        BODY
+      </main>
+      <footer className='footer'>
+        All imagery, music, trademarks etc. are © Copyright their respective owners. All rights reserved. 
+      </footer>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styled-components/Nav.css';
+import logo from '../images/knicks-logo.png';
 
 const Nav = () => {
     return (
-        <div>
-            <Link to="">
-            <h1>Home</h1>
+        <div className="Nav">
+            <Link to="/" className='home-link'>
+            <img src={logo} className='logo' alt='logo'/>
             </Link>
 
-            <Link to="">
+            <Link to="" className='player-stats-link'>
             <h1>Player Stats</h1>
+            </Link>
+
+            <Link to="" className='team-stats-link'>
+            <h1>Team Stats</h1>
+            </Link>
+
+            <Link to="" className='recent-games-link'>
+            <h1>Games</h1>
             </Link>
         </div>
     );
